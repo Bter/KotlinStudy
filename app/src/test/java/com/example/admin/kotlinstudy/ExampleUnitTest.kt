ef4734852ae5c174d37cbb07513ea5e9af158989
peter <renrenaiming@126.com>
2018-06-27 10:03:09 +0800

    add project file.

---
+++ app/src/test/java/com/example/admin/kotlinstudy/ExampleUnitTest.kt
@@ -0,0 +1,30 @@
+package com.example.admin.kotlinstudy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.util.ArrayList
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+//        assertEquals(4, 2 + 2)
+        println(sum(1,3))
+        var a:Boolean = false
+        a = true
+        println(a)
+        val args:ArrayList<String> = ArrayList()
+        args.add("1")
+        printInt("aaa").toString()
+    }
+
+    fun sum(a: Int, b:Int) = (a + b).toString()
+    fun printInt(str:String): Int?{
+        return null
+    }
+}
